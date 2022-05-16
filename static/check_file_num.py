1651c9f4d6a559b8883a95e3d620a2669daacbe7
jeochris <chris990126@gmail.com>
2022-05-16 12:26:07 +0900

    modify gitignore

--- static/check_file_num.py
+++ static/check_file_num.py
@@ -2,17 +2,17 @@ import glob
 
 for i in range(1,2001):
     imgs_path_list = glob.glob(f'./front/comp{i}/*')
-    if len(imgs_path_list) < 2:
+    if len(imgs_path_list) != 2:
         print(i)
 
 print()
 for i in range(1,801):
     imgs_path_list = glob.glob(f'./left/comp{i}/*')
-    if len(imgs_path_list) < 2:
+    if len(imgs_path_list) != 2:
         print(i)
 
 print()
 for i in range(1,801):
     imgs_path_list = glob.glob(f'./right/comp{i}/*')
-    if len(imgs_path_list) < 2:
+    if len(imgs_path_list) != 2:
         print(i)
